d4c2a064aa2a81ef5bf919925785510332c58b44
Actireon <eheieiei37@gmail.com>
2020-02-11 18:24:33 +0200

    Updates

---
+++ precache-manifest.0e1d7f562573b2b6337dc367eeef786e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24faecbee1095199aebad5e01f6207c8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bfd6d8254abe19d5b4fc",
+    "url": "./static/css/main.58d7e4a9.chunk.css"
+  },
+  {
+    "revision": "5fdac0a2c9586d9d1584",
+    "url": "./static/js/2.6b91d4b6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.6b91d4b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bfd6d8254abe19d5b4fc",
+    "url": "./static/js/main.690e0bf6.chunk.js"
+  },
+  {
+    "revision": "e67b79006193623cc7c8",
+    "url": "./static/js/runtime-main.a19c704a.js"
+  }
+]);
